2cbb5d44edad4401bacc25bee3d93a7835f01a91
Yogesh Gupta <69033552+yogeshgupta279@users.noreply.github.com>
2020-11-21 18:51:57 +0530

    Update functions.php

--- Components/Texts/Functions/functions.php
+++ Components/Texts/Functions/functions.php
@@ -1,6 +1,7 @@
 <?php
 	//MySQL Database Connection
 	$con = mysqli_connect("localhost","root","","societypro") or die("Error in Connection: " . mysqli_connect_error());
+	//mysqli_connect("sql305.epizy.com","epiz_27233228","kofjisjcOPUE","epiz_27233228_societypro")
 	
 	//Secretary SignIn Functinality
 	function secretarySignIn(){
