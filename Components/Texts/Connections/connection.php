2e1700a4b6b212ad15495a1970cd7c656bd52cf3
Yogesh Gupta <69033552+yogeshgupta279@users.noreply.github.com>
2020-11-21 18:51:25 +0530

    Update connection.php

--- Components/Texts/Connections/connection.php
+++ Components/Texts/Connections/connection.php
@@ -1,4 +1,5 @@
 <?php
 	//MySQL Database Connection
 	$con = mysqli_connect("localhost","root","","societypro") or die("Error in Connection: " . mysqli_connect_error());
+	//mysqli_connect("sql305.epizy.com","epiz_27233228","kofjisjcOPUE","epiz_27233228_societypro")
 ?>
